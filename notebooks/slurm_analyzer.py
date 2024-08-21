90d6dabb0b9496d3a8d29aed923ee9f52b726eeb
Kilian Lieret <kilian.lieret@posteo.de>
2024-08-21 22:03:26 +0200

    Analyze wait times

---
+++ notebooks/slurm_analyzer.py
@@ -0,0 +1,44 @@
+from pandas import DataFrame as DF
+import pandas as pd
+
+
+class SLURMAnalyzer:
+    def parse(self, data: dict) -> DF:
+        records = []
+        for job in data["jobs"]:
+            n_gpus = self._get_n_gpus(job)
+            record = {
+                "n_gpus": n_gpus,
+                "elapsed": job["time"]["elapsed"],
+                "start_time": job["time"]["start"],
+                "submission_time": job["time"]["submission"],
+            }
+            for key in ["qos", "account", "partition", "qos", "user"]:
+                record[key] = job[key]
+            records.append(record)
+        df = pd.DataFrame.from_records(records)
+        self._augment_df(df)
+        df = self._sanity_filter(df)
+        return df
+
+
+    @staticmethod 
+    def _get_n_gpus(job_data: dict) -> int:
+        n_gpus = 0
+        for allocation in job_data["tres"]["allocated"]:
+            if allocation["type"] == "gres" and allocation["name"] == "gpu":
+                n_gpus += int(allocation["count"])
+        return n_gpus
+    
+    @staticmethod
+    def _augment_df(df: DF) -> None:
+        df["gpu_time"] = df["n_gpus"] * df["elapsed"]
+        df["wait_time"] = df["start_time"] - df["submission_time"]
+        for c in ["start_time", "submission_time"]:
+            df[c] = pd.to_datetime(df[c], unit="s")
+   
+    @staticmethod
+    def _sanity_filter(df: DF) -> DF:
+        # Less than 1 month wait time
+        return df[df["wait_time"] < 3600*24*31]
+    
